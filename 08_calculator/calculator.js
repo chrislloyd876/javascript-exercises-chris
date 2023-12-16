fe359b33c9bc88cd8a6506d8f2705eaea02b6fd1
chrislloyd876 <chrislloydlawrence@gmail.com>
2023-12-16 13:12:40 -0500

    Complete Palindrome exercise
    Completed both calculator and palindrome exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,24 +3,42 @@ const add = function(num1, num2) {
   return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let answer = num1 - num2;
+  return answer;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let answer = 0;
+  for(let count = 0; count < array.length; count++){
+    answer += array[count];
+  }
+  return answer;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let answer = 1;
+  for(let count = 0; count < array.length; count++){
+    answer *= array[count];
+  }
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	let answer = 1;
+  for(let count = 0; count < pow; count++){
+    answer *= num;
+  }
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0){
+    return 1;
+  }
+  else {
+    return num * factorial(num -1);
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const newPhrase = phrase.toLowerCase().replace(/[^a-z0-9]/g,'');
+    const reversePhrase = newPhrase.split('').reverse().join('');
+    return newPhrase == reversePhrase;
 };
 
 // Do not edit below this line
